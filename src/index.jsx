4494ec578c34d9ec9c7ce2a5c961d1e5fdc39cb5
Lucca <lgursog13@gmail.com>
2023-08-03 17:33:17 -0300

    Tela de Cadastro Finalizada

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TelaCadastro from './pages/App_TelaCadastro';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TelaCadastro />
+  </React.StrictMode>
+);
---
+++ src/styled components/image.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ImgTop = styled.img`
+    width: 360px;
+    height: 240px;
+    transform: rotate(180deg);
+`
+
+const ImgBot = styled.img`
+    width: 350px;
+    height: 235px;
+    margin-left: 394px;
+`
+
+export { ImgTop, ImgBot }
